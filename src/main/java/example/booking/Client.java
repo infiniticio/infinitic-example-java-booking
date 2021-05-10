6df860d904910e1e95039bc5d34be14372f998a8
Gilles Barbier <gilles@zenaton.com>
2021-05-10 19:20:03 +0200

    update to 0.6.1

--- src/main/java/example/booking/Client.java
+++ src/main/java/example/booking/Client.java
@@ -4,12 +4,13 @@ import example.booking.tasks.carRental.CarRentalCart;
 import example.booking.tasks.flight.FlightBookingCart;
 import example.booking.tasks.hotel.HotelBookingCart;
 import example.booking.workflows.BookingWorkflow;
-import io.infinitic.pulsar.InfiniticClient;
+import io.infinitic.client.InfiniticClient;
+import io.infinitic.pulsar.PulsarInfiniticClient;
 
 public class Client {
     public static void main(String[] args) {
         // instantiate Infinitic client based on infinitic.yml config file
-        io.infinitic.client.Client client = InfiniticClient.fromConfigFile("configs/infinitic.yml");
+        InfiniticClient client = PulsarInfiniticClient.fromConfigFile("configs/infinitic.yml");
 
         // faking some carts
         CarRentalCart carRentalCart = new CarRentalCart();
--- src/main/java/example/booking/Worker.java
+++ src/main/java/example/booking/Worker.java
@@ -1,10 +1,10 @@
 package example.booking;
 
-import io.infinitic.pulsar.InfiniticWorker;
+import io.infinitic.pulsar.PulsarInfiniticWorker;
 
 public class Worker {
     public static void main(String[] args) {
         String file = args.length>0 ? args[0] : "configs/all.yml";
-        InfiniticWorker.fromConfigFile(file, "configs/infinitic.yml").start();
+        PulsarInfiniticWorker.fromConfigFile(file, "configs/infinitic.yml").start();
     }
 }
